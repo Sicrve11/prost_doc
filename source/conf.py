46072e00e4bd8a28e7275bf88628b73a9e147fe8
Shigw <120349754+Sicrve11@users.noreply.github.com>
2023-05-09 11:26:31 +0800

    Update

--- source/conf.py
+++ source/conf.py
@@ -28,8 +28,8 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-'.md': CommonMarkParser,
-}
-source_suffix = ['.rst', '.md']
+# from recommonmark.parser import CommonMarkParser
+# source_parsers = {
+# '.md': CommonMarkParser,
+# }
+# source_suffix = ['.rst', '.md']
